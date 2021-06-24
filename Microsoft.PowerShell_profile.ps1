bd4a18657d3c4816bca081832b7eb21517aba89c
Read <decimation001@gmail.com>
2021-06-24 03:52:27 -0500

    Add files via upload

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,74 +1,3 @@
-
-function Push-All {
-	param(
-        [Parameter(Mandatory=$false)][string]$dest
-    )
-	
-	$cd = Get-Location
-
-	if (!($dest)) {
-		$dest="sdcard/"
-	}
-
-	#$dest = $(If ($args.Count -eq 0) {"sdcard/"} Else {$dest})
-
-	Write-Host $cd files to $dest
-
-	Get-ChildItem | ForEach-Object {
-		if ([System.IO.File]::Exists($_)) {
-			adb push $_ $dest
-		}
-	}
-}
-
-function Pull-All-Filter {
-	<#[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs')]#>
-	[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs', '', Scope='Function', Target='*')]
-	param (
-		[Parameter(Mandatory=$true)][string]$src,
-		[Parameter(Mandatory=$true)][string]$f
-	)
-	foreach ($x in (adb shell ls $src | Select-String $f)) {
-		adb pull "$src/$x"
-	}
-}
-function Input-Tap {
-	<#[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs')]#>
-	[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs', '', Scope='Function', Target='*')]
-	param (
-		[Parameter(Mandatory=$true)][long]$x,
-		[Parameter(Mandatory=$true)][long]$y
-	)
-	adb shell input tap $x $y
-}
-
-<#function kuroba {
-	
-	Input-Tap 1360 235 
-	Input-Tap 1043 1244
-	Input-Tap 530 2223 
-	Input-Tap 500 1137 
-	Input-Tap 1190 250
-
-	
-
-}#>
-
-<# adb shell input swipe 50 1330 1370 1430 10 #>
-
-<#function DecodeBase64Image {
-    param (
-        [Parameter(Mandatory=$true)]
-        [String]$ImageBase64
-    )
-    # Parameter help description
-    $ObjBitmapImage = New-Object System.Windows.Media.Imaging.BitmapImage #Provides a specialized BitmapSource that is optimized for loading images using Extensible Application Markup Language (XAML).
-    $ObjBitmapImage.BeginInit() #Signals the start of the BitmapImage initialization.
-    $ObjBitmapImage.StreamSource = [System.IO.MemoryStream][System.Convert]::FromBase64String($ImageBase64) #Creates a stream whose backing store is memory.
-    $ObjBitmapImage.EndInit() #Signals the end of the BitmapImage initialization.
-    $ObjBitmapImage.Freeze() #Makes the current object unmodifiable and sets its IsFrozen property to true.
-}#>
-
 function Get-Translation {
 	param (
 		[Parameter(Mandatory=$true)][string]$x,
@@ -76,7 +5,7 @@ function Get-Translation {
 	)
 
 	$cmd = "from googletrans import * `n" +`
-	"tmp=Translator().translate('$x',dest='$y')`n" +`
+	"tmp = Translator().translate('$x', dest='$y')`n" +`
 	"print(tmp.text)`n" +`
 	"print(tmp.pronunciation)"
 
@@ -85,7 +14,7 @@ function Get-Translation {
 	Write-Host
 
 	$cmd2 = "from translatepy import * `n" +`
-	"tmp2=Translator().translate('$x','$y')`n" +`
+	"tmp2 = Translator().translate('$x', '$y')`n" +`
 	"print(tmp2)"
 
 	python -c $cmd2
@@ -106,11 +35,12 @@ function Prompt {
 	
 	Write-Host ("PS " + "[$(Get-Date -Format "HH:mm:ss")] " + $(Get-Location) +">") -NoNewLine
 	return " "
-
-  }
+}
 
 #Remove-Module -Name Index
+
 Import-Module "$Home\Documents\PowerShell\Modules\Index.psm1"
+Import-Module "$Home\Documents\PowerShell\Modules\Android.psm1"
 
 # .
 # &
