08ede07333693be2bf1cb7a56096441f7039dbaf
Read <decimation001@gmail.com>
2021-07-05 21:01:47 -0500

    Add files via upload

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -41,26 +41,76 @@ function Prompt {
 	return " "
 }
 
+function Set-Constant {
+	<#
+	  .SYNOPSIS
+		  Creates constants.
+	  .DESCRIPTION
+		  This function can help you to create constants so easy as it possible.
+		  It works as keyword 'const' as such as in C#.
+	  .EXAMPLE
+		  PS C:\> Set-Constant a = 10
+		  PS C:\> $a += 13
+  
+		  There is a integer constant declaration, so the second line return
+		  error.
+	  .EXAMPLE
+		  PS C:\> const str = "this is a constant string"
+  
+		  You also can use word 'const' for constant declaration. There is a
+		  string constant named '$str' in this example.
+	  .LINK
+		  Set-Variable
+		  About_Functions_Advanced_Parameters
+	#>
+	[CmdletBinding()]
+	param(
+	  [Parameter(Mandatory=$true, Position=0)]
+	  [string][ValidateNotNullOrEmpty()]$Name,
+  
+	  [Parameter(Mandatory=$true, Position=1)]
+	  [char][ValidateSet("=")]$Link,
+  
+	  [Parameter(Mandatory=$true, Position=2)]
+	  [object][ValidateNotNullOrEmpty()]$Mean,
+  
+	  [Parameter(Mandatory=$false)]
+	  [string]$Surround = "script"
+	)
+  
+	Set-Variable -n $name -val $mean -opt Constant -s $surround
+}
+  
+Set-Alias const Set-Constant
+
+function Import-Deci {
+	
+	Import-Module "$Home\Documents\PowerShell\Modules\Index.psm1"
+	Import-Module "$Home\Documents\PowerShell\Modules\Android.psm1"
+	
+}
+
+function Remove-Deci {
+	Remove-Module Index
+	Remove-Module Android
+	
+}
+
+
+function Import-Profile {
+	. $PROFILE
+}
+
 #region [Modules]
 
 #Remove-Module -Name Index
 
-Import-Module "$Home\Documents\PowerShell\Modules\Index.psm1"
-Import-Module "$Home\Documents\PowerShell\Modules\Android.psm1"
+Import-Deci
 
 #endregion
-
-#region [Aliases]
-
-#New-Alias -Name wh -Value Write-Host
-
-Set-Alias -Name wh -Value Write-Host
-Set-Alias -Name ytdl -Value youtube-dl
-
-#endregion
-
 
 #$WarningPreference = "SilentlyContinue"
 
-# .
-# &
+# Chain commands	;
+# Dot sourcing		.
+# Call				&
--- Modules/Index.psm1
+++ Modules/Index.psm1
@@ -4,5 +4,6 @@
 
 Set-Alias -Name wh -Value Write-Host
 Set-Alias -Name ytdl -Value youtube-dl
+Set-Alias -Name fg -Value ffmpeg
 
 #endregion
