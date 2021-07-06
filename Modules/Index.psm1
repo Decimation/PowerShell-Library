700c45e91aa0c9a3478e6d2c9a8a64e31e0c83ba
Read <decimation001@gmail.com>
2021-07-06 03:19:37 -0500

    Update

--- Modules/Index.psm1
+++ Modules/Index.psm1
@@ -1,5 +1,11 @@
+<#
+# General utilities
+#>
+
 <#----------------------------------------------------------------------------#>
 
 Set-Alias -Name wh -Value Write-Host
+Set-Alias -Name wd -Value Write-Debug
+
 Set-Alias -Name ytdl -Value youtube-dl
 Set-Alias -Name fg -Value ffmpeg
