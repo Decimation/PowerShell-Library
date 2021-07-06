33b8a02a8b82a32185d532a92ec065b16a4dc655
Read <decimation001@gmail.com>
2021-07-06 03:04:29 -0500

    msg

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -159,8 +159,8 @@ function Send-GitHubFile {
 	$tokenEnv = [System.Environment]::GetEnvironmentVariable("GH_TOKEN")
 	AutoAssign([ref]$token) -val $tokenEnv
 	
-	Write-Host "Name: $name`n" `+
-	"Token: $token"
+	Write-Host "Name: $name"
+	Write-Host "Token: $token"
 
 	$url = "https://api.github.com/repos/$name/$repoName/contents/$fileName"
 
