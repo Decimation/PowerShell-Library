204d817969533609c6377ff6ff5ef1ec94ab6743
Read Stanton <decimation001@gmail.com>
2021-09-28 02:05:59 -0500

    Add files via upload

--- Scripts/Wikipedia formulas.ps1
+++ Scripts/Wikipedia formulas.ps1
@@ -13,49 +13,35 @@ for ($i = 0; $i -lt $s.Length; $i++) {
 	$b1 = $s[$i].ToString().Replace('\displaystyle', '').Trim('{').Trim('}').Trim()
 	$b1 = $b1.Replace('}{', "`n").Trim()
 
-	
 	$b1 = Parse($b1)
 
-	#($rx[-1].Split("`n ")[0].Trim()) -eq ($rx[-1].Split("`n ")[1].Trim())
-
-
-	<# $b2 = $b1.Split("`n ")
-	
-	if ($b2[0].Trim() -eq $b2[1].Trim()) {
-		#$l[$i] = $b2
-		$b1 = $b2
-	}
-	
-	else {
-		
-	} #>
 	$b1 = $b1.Trim().TrimStart([char]32)
 	$b2 = $b1.Split("`n ")
-	
+
 	if ($b2.Length -eq 2 -and ($b2[0].Trim() -eq $b2[1].Trim())) {
 		#$l[$i] = $b2
 		$b1 = $b2[0]
 	}
-	$l[$i] = ($b1)
 
+	$l[$i] = ($b1)
 }
 
 function Parse($b1) {
-	
 
-	#$b1 = [regex]::Replace($b1, '\^{\w}', '^C')
 	$b1 = $b1.Replace('^{C}', '^C')
 	$b1 = $b1.Replace('^{C', '^C')
 
 	$b1 = $b1.Replace('\varnothing', '\emptyset')
-	
 	$b1 = $b1.Trim().TrimStart([char]32)
+
 	return $b1
 }
+
 if ($replace) {
 	Set-Clipboard $l
 	Write-Host 'Replaced clipboard'
 }
 
 $l = $l | Select-Object -Unique
+
 return $l
