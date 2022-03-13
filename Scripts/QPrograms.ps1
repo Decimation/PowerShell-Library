d2e0e51d460e496ec1ede0e0d2639f722824bff9
Read <decimation001@gmail.com>
2022-03-13 03:46:17 -0500

    Update QPrograms.ps1

--- Scripts/QPrograms.ps1
+++ Scripts/QPrograms.ps1
@@ -21,6 +21,8 @@ class PackageManager : BackupSource {
 	[string]$uninstall
 	[string]$update
 	[string]$list
+
+	
 	
 	[void]Clear() {
 		$this.GetType().GetFields()
@@ -31,7 +33,9 @@ $Index = @(
 	[PackageManager]@{
 		name = 'scoop'
 		run  = { 
-			scoop export | Out-File 'scoop.txt'
+			Write-Debug "$($args -join ',')"
+			$dir = $args[0]
+			scoop export | Out-File "$dir\scoop.txt"
 		}
 	},
 	[PackageManager]@{
@@ -39,63 +43,75 @@ $Index = @(
 		search  = '-Q'
 		install = '-S'
 		run     = {
+			$dir = $args[0]
+
 			if ((Get-Command -Name pacman)) {
-				Write-Host 'Export: pacman'
-				pacman -Q | Out-File 'pacman.txt'
+				pacman -Q | Out-File "$dir\pacman.txt"
 			}
 		}
 	},
 	[PackageManager]@{
 		name = 'choco'
 		run  = {
-			choco export 'choco.config' --include-version-numbers | Out-Null
+			$dir = $args[0]
+
+			choco export "$dir\choco.config" --include-version-numbers | Out-Null
 		}
 	},
 	[PackageManager]@{
 		name   = 'pip'
 		update = (($pm_install + ' --upgrade'))
 		run    = {
-			pip list | Out-File 'pip.txt'
+			$dir = $args[0]
+
+			pip list | Out-File "$dir\pip.txt"
 		}
 	},
 	[PackageManager]@{
 		name = 'npm'
 		run  = {
-			npm list -g | Out-File 'npm.txt'
+			$dir = $args[0]
+
+			npm list -g | Out-File "$dir\npm.txt"
 		}
 	},
 	[PackageManager]@{
 		name = 'winget'
 		run  = {
-			winget export 'winget.json' --include-versions | Out-Null
+			$dir = $args[0]
+
+			winget export "$dir\winget.json" --include-versions | Out-Null
 		}
 	},
 	[BackupSource]@{
 		name = 'appx'
 		run  = {
+			$dir = $args[0]
+
 			if ((Get-Command -Name Invoke-WinCommand)) {
 				Invoke-WinCommand {
 					param ($arg0)
-					Get-AppxPackage | Out-File 'apps.txt'
+					Get-AppxPackage | Out-File "$dir\apps.txt"
 				} -ArgumentList $ds
 		
-				Write-Host 'Export: Appx'
 		
 				Invoke-WinCommand {
 					param ($arg0)
-					Export-StartLayout 'start layout.xml'
+					
+					Export-StartLayout "$dir\start layout.xml"
 				} -ArgumentList $ds
-				Write-Host 'Export: Start layout'
 			}
 		}
 	},
 	[BackupSource]@{
 		name = 'bd'
 		run  = {
+			$dir = $args[0]
+
 			$bd = "$env:APPDATA\BetterDiscord"
 			if ((Test-Path $bd)) {
-				mkdir 'BetterDiscord' | Out-Null
-				Get-ChildItem "$bd\plugins" | Out-File 'BetterDiscord plugins list.txt'
+				mkdir "$dir\BetterDiscord" | Out-Null
+				Get-ChildItem "$bd\plugins" | Out-File "$dir\BetterDiscord\BetterDiscord plugins list.txt"
 				robocopy "$bd\data\stable" '.' | Out-Null
 			}
 		}
@@ -103,21 +119,21 @@ $Index = @(
 	[BackupSource]@{
 		name = 'env'
 		run  = {
+			$dir = $args[0]
+			
 			$lm = Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment'
 			$cu = Get-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment'
 	
-			Write-Host 'Export: Environment variables (machine)'
-			Write-Host 'Export: Environment variables (user)'
-	
-			$lm | Out-File 'env variables (machine).txt'
-			$cu | Out-File 'env variables (user).txt'
+			$lm | Out-File "$dir\env variables (machine).txt"
+			$cu | Out-File "$dir\env variables (user).txt"
 		}
 	}
 	[BackupSource]@{
 		name = 'progfiles'
 		run  = {
-			Get-ChildItem $env:ProgramFiles | Out-File 'programs.txt'
-			Get-ChildItem ${env:ProgramFiles(x86)} | Out-File 'programs (x86).txt'
+			$dir = $args[0]
+			Get-ChildItem $env:ProgramFiles | Out-File "$dir\programs.txt"
+			Get-ChildItem ${env:ProgramFiles(x86)} | Out-File "$dir\programs (x86).txt"
 		}
 	}
 )
@@ -128,9 +144,17 @@ Write-Debug "$($args -join ',')"
 switch ($args[0]) {
 	'run' {
 		$n = $args[1]
-		$op = $Index | Where-Object { $_.name -eq $n }
-		Write-Debug "$($op.name)"
-		& $op.run
+		$op = $Index | Where-Object { $_.name -match $n }
+		foreach ($a in $op) {
+
+			if (-not (Test-Path $ds)) {
+				mkdir $ds
+			}
+			
+			Write-Host ">> $ds"
+			Write-Host "$($a.name)"
+			& $a.run $ds
+		}
 	}
 	
 	Default {
