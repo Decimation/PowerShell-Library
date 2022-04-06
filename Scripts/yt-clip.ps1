5b22876272c1579f48ccd3e8c14429a16d8fd97a
Read <decimation001@gmail.com>
2022-04-06 16:01:59 -0500

    2022-04-06 16:01:59

---
+++ Scripts/yt-clip.ps1
@@ -0,0 +1,46 @@
+param (
+	$url, $s, $e, $outFile,
+	[Parameter(Mandatory = $false)]
+	$other
+)
+
+
+function Get-TimeDuration {
+	param (
+		[Parameter(Mandatory = $true)][timespan]$a,
+		[Parameter(Mandatory = $true)][timespan]$b
+	)
+
+	$a = [timespan]::Parse($a)
+	$b = [timespan]::Parse($b)
+	$c = ($a - $b)
+	$c = [timespan]::FromTicks([System.Math]::Abs($c.Ticks))
+
+	return $c;
+}
+
+$a = @('-g', '--youtube-skip-dash-manifest', $url)
+$x = yt-dlp @a
+
+$v = $x[0]
+$a = $x[1]
+$d = Get-TimeDuration ([timespan]::Parse($s)) ([timespan]::Parse($e))
+
+$argb = @('-ss', $s, '-i', $v)
+
+
+if ($a) {
+	$argb += @('-ss', $s, '-i', $a)
+}
+
+$argb += @('-t', $d)
+$argb += @("-map", "0:v", "-map", "1:a")
+$argb += @("-c:v", "libx264", "-c:a", "aac", $outFile)
+
+Write-Host "$d"
+Write-Host "$($argb -join ' ')"
+Read-Host -Prompt "..."
+
+$p = Start-Process -FilePath 'ffmpeg.exe' -RedirectStandardOutput:$true `
+	-ArgumentList $argb -NoNewWindow
+
