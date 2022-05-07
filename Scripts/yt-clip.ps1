6e823cd65127bf6b282a6f168171590c6804ed32
Read <decimation001@gmail.com>
2022-05-07 18:31:34 -0500

    2022-05-07 18:31:34

--- Scripts/yt-clip.ps1
+++ Scripts/yt-clip.ps1
@@ -4,38 +4,33 @@
 Downloads a YouTube clip with yt-dlp and ffmpeg
 .LINK
 https://gist.github.com/lostfictions/5700848187b8edfb6e45270b462a4534
+.EXAMPLE
+& "yt-clip.ps1" -Url "https://www.youtube.com/watch?v=SNgNBsCI4EA" -Start '1:10' -End "1:26"
+.EXAMPLE
+& "yt-clip.ps1" -Url "https://youtu.be/YPqYvll6XD0" -Start '10:52' -End "11:38" -Args2 @('-preset','veryfast')
 #>
 param (
+	# Url
+	[alias('u')]
 	$Url, 
+	# Start time
+	[Alias('s')]
 	$Start = '0:0:0', 
+	# End time
+	[alias('e')]
 	$End,
 	[Parameter(Mandatory = $false)]
 	$Output = $null,
+	# yt-dlp args
 	[Parameter(Mandatory = $false)]
-	$Other, 
+	$Args1,
+	# ffmpeg args
 	[Parameter(Mandatory = $false)]
-	$Other2 = @('-y')
+	$Args2 = @('-y'),
+	[Alias('cf')][switch]$Confirm
 )
 
-$arg2 = @($Url, '--print', 'id')
-$Output ??= "out_$(yt-dlp @arg2).mp4"
-Write-Host "Automatic output filename: $Output"
-
-if (Test-Path $Output) {
-	$yn = Read-Host -Prompt "$Output already exists. Remove? [y/n/a]"
-	switch ($yn) {
-		'y' {
-			Remove-Item $Output
-		}
-		'n' {
-			
-		}
-		'a' {
-			return
-		}
-	}
-}
-
+#$ErrorActionPreference = 'Abort'
 
 function script:Get-ParsedTime($t) {
 	$st = $t.Split(':')
@@ -65,10 +60,79 @@ function script:Get-ParsedTime($t) {
 $Start = script:Get-ParsedTime($Start)
 $End = script:Get-ParsedTime($End)
 
-Write-Host "Start: $Start"
-Write-Host "End: $End"
+Write-Host "Start: $Start" -ForegroundColor 'Cyan'
+Write-Host "End: $End" -ForegroundColor 'Cyan'
 
-$arg1 = @('-g', $Other, '--youtube-skip-dash-manifest', $Url)
+$e_ffmpeg = 'ffmpeg'
+$e_ytdlp = 'yt-dlp'
+
+$c_ytdlp = (Get-Command $e_ytdlp)
+$c_ffmpeg = (Get-Command $e_ffmpeg)
+
+if (-not $c_ytdlp) {
+	Write-Error "$e_ytdlp not found"
+	return
+}
+
+if (-not $c_ffmpeg) {
+	Write-Error "$e_ffmpeg not found"
+	return
+}
+
+Write-Host "$($c_ytdlp.Path)" -ForegroundColor 'DarkGrey'
+Write-Host "$($c_ffmpeg.Path)" -ForegroundColor 'DarkGrey'
+
+# $tf = "hh\.mm\.ss"
+$tf = "hh\hmm\mss\s"
+
+$fs = $Start.ToString($tf)
+$fe = $End.ToString($tf)
+
+# $arg2 = @($Url, '--print', 'id')
+$arg2 = @($Url, '--print', 'title', '--restrict-filenames')
+
+$Output ??= "$(yt-dlp @arg2) ($fs - $fe).mp4"
+Write-Host "Output filename: $Output" -ForegroundColor 'Green'
+
+if (Test-Path $Output) {
+	$yn = Read-Host -Prompt "$Output already exists. Remove? [y/n/a]"
+	switch ($yn) {
+		'y' {
+			Remove-Item $Output
+		}
+		'n' {
+			
+		}
+		'a' {
+			return
+		}
+	}
+}
+
+
+function script:Read-Confirmation {
+	
+	if ($Confirm) {
+		switch (Read-Host "Continue? [y/n] (or press enter)") {
+			default {
+				
+			}
+			'y' {
+				
+			}
+			'n' {
+				exit
+			}
+		}
+	}
+
+}
+
+script:Read-Confirmation
+
+# yt-dlp args
+
+$arg1 = @('-g', $Args1, '--youtube-skip-dash-manifest', $Url)
 $x1 = yt-dlp @arg1
 
 $video = $x1[0]
@@ -84,13 +148,18 @@ if ($audio) {
 $ffArgs += @('-t', $duration, `
 		"-map", "0:v", "-map", "1:a", `
 		"-c:v", "libx264", "-c:a", "aac") `
-	+ $Other2 + "`"$Output`""
+	+ $Args2 + "`"$Output`""
 
 
-Write-Host "$Start - $End ($duration)"
+Write-Host "Duration: ($duration)" -ForegroundColor 'DarkGray'
 Write-Debug "$($ffArgs -join ' ')"
 
+script:Read-Confirmation
+
 ffmpeg @ffArgs
+$of = $(Resolve-Path $Output)
+
+Write-Host "Output: $of" -ForegroundColor 'Green'
 
 <# $p = Start-Process -FilePath 'ffmpeg.exe' -RedirectStandardOutput:$true `
 	-ArgumentList $ffArgs -NoNewWindow -PassThru
