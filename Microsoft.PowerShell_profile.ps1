98dfd558bf0c973e91420508dd00537f0e533d5d
Read <decimation001@gmail.com>
2022-07-03 23:20:02 -0500

    2022-07-03 23:20:02

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -545,44 +545,40 @@ Set-PSReadLineKeyHandler -Key Backspace `
 
 # This example will replace any aliases on the command line with the resolved commands.
 Set-PSReadLineKeyHandler -Key "Alt+%" `
-                         -BriefDescription ExpandAliases `
-                         -LongDescription "Replace all aliases with the full command" `
-                         -ScriptBlock {
-    param($key, $arg)
+	-BriefDescription ExpandAliases `
+	-LongDescription "Replace all aliases with the full command" `
+	-ScriptBlock {
+	param($key, $arg)
 
-    $ast = $null
-    $tokens = $null
-    $errors = $null
-    $cursor = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)
+	$ast = $null
+	$tokens = $null
+	$errors = $null
+	$cursor = $null
+	[Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)
 
-    $startAdjustment = 0
-    foreach ($token in $tokens)
-    {
-        if ($token.TokenFlags -band [TokenFlags]::CommandName)
-        {
+	$startAdjustment = 0
+	foreach ($token in $tokens) {
+		if ($token.TokenFlags -band [TokenFlags]::CommandName) {
 			
-            $alias = $ExecutionContext.InvokeCommand.GetCommand($token.Extent.Text, 'Alias')
-            if ($alias -ne $null)
-            {
-                $resolvedCommand = $alias.ResolvedCommandName
-                if ($resolvedCommand -ne $null)
-                {
-                    $extent = $token.Extent
-                    $length = $extent.EndOffset - $extent.StartOffset
-                    [Microsoft.PowerShell.PSConsoleReadLine]::Replace(
-                        $extent.StartOffset + $startAdjustment,
-                        $length,
-                        $resolvedCommand)
+			$alias = $ExecutionContext.InvokeCommand.GetCommand($token.Extent.Text, 'Alias')
+			if ($alias -ne $null) {
+				$resolvedCommand = $alias.ResolvedCommandName
+				if ($resolvedCommand -ne $null) {
+					$extent = $token.Extent
+					$length = $extent.EndOffset - $extent.StartOffset
+					[Microsoft.PowerShell.PSConsoleReadLine]::Replace(
+						$extent.StartOffset + $startAdjustment,
+						$length,
+						$resolvedCommand)
 
-                    # Our copy of the tokens won't have been updated, so we need to
-                    # adjust by the difference in length
-                    $startAdjustment += ($resolvedCommand.Length - $length)
-                }
+					# Our copy of the tokens won't have been updated, so we need to
+					# adjust by the difference in length
+					$startAdjustment += ($resolvedCommand.Length - $length)
+				}
 
-            }
-        }
-    }
+			}
+		}
+	}
 }
 
 
@@ -630,3 +626,6 @@ if ($env:TERM_PROGRAM -eq 'vscode') {
 
 #New-Item -ItemType SymbolicLink -Target .\Microsoft.PowerShell_profile.ps1 -Force .\Microsoft.VSCode_profile.ps1
 # endregion
+
+Import-Module "$(get-item ((Find-Item gsudo)[0])|^ -exp Directory)\gsudoModule.psd1"
+$gsudoLoadProfile = $true
