9586a46f0d8949591a726b15a3626770bf389c1f
Read <decimation001@gmail.com>
2022-11-11 03:21:43 -0600

    2022-11-11 03:21:43

--- Scripts/QPackages.ps1
+++ Scripts/QPackages.ps1
@@ -1,26 +1,53 @@
 #Requires -Module PSKantan
 
-$PackageManagers = @('scoop', 'winget')
+$PackageManagers = @(
+	@{
+		name   = 'scoop'
+		search = 'search'
+	},
+	@{
+		name   = 'winget'
+		search = 'search'
+	},
+	@{
+		name   = 'choco'
+		search = 'search'
+	},
+	@{
+		name   = 'pacman'
+		search = '-Q'
+	}
+)
 
 function Search-Q {
 	param (
 		$Value
 	)
-	
+
 	$jobs = @()
+
 	for ($i = 0; $i -lt $PackageManagers.Count; $i++) {
-		<# Action that will repeat until the condition is met #>
 		$pm = $PackageManagers[$i]
+		$pmn = $pm.name
+		$in = @($pm.search, $Value)
+		Write-Debug "[$pmn] $UNI_ARROW_LEFT $in"
 		$j = Start-Job -ScriptBlock {
-			& $using:pm $using:Value
-		} -Name "[search] $pm"
+			& $using:pmn @using:in
+		} -Name "[$($in[0])] $pm"
 		$jobs += $j
 	}
 
 
+	
+	return $jobs
+}
+
+function Receive-Q {
+	param (
+		$jobs
+	)
 	foreach ($j in $jobs) {
-		$o = Receive-Job $j -Wait
+		$o = Receive-Job -Job $j -Wait
 		Write-Host "$o"
 	}
-
 }
