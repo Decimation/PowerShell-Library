5c54d04c28e4c135feac24588f2ebcf5de08ff3f
Read <decimation001@gmail.com>
2024-06-29 01:22:16 -0500

    Update

--- Scripts/PS-Audiosurf.ps1
+++ Scripts/PS-Audiosurf.ps1
@@ -1,54 +1,79 @@
 
+
+<#
+.DESCRIPTION
+	Detects the song name from an Audiosurf 2 replay file.
+#>
 function Get-SongName {
 	param (
+		[Parameter(Mandatory = $true)]
 		$Path,
 		$Seek = 15
 	)
 
-
-	<# $tmp = [System.IO.Path]::GetTempFileName()
-	$tmp = [System.IO.Path]::ChangeExtension($tmp, 'png')
-	$tmp2 = [System.IO.Path]::GetTempFileName()
-	$tmp2 = [System.IO.Path]::ChangeExtension($tmp2, 'png') #>
-	
 	$hsh = $Path.GetHashCode()
-	$tmp = "Frame_$hsh.png"
+	$TmpFile = "Frame_$hsh.png"
 
-	Write-Verbose "Temp file 1: $tmp | Temp file 2: $tmp2"
 
-	$ff = ffmpeg -ss $Seek -i "$Path" -vf "select=eq(n\,34),crop=x=870:y=1050:w=680:h=50" -vframes 1 -f image2pipe "$tmp" -y *>&1
-	$name = tesseract -l eng "$tmp" stdout 2>nul
+	Write-Verbose "Temp file 1: $TmpFile"
 
-	$name = $name -is [array] ? $name[-1] : $name
-	$name = $name.Trim()
-	$name = $name -replace '\s+', ' '
+	$ff = ffmpeg -ss $Seek -i "$Path" -vf "select=eq(n\,34),crop=x=870:y=1050:w=1000:h=50" -vframes 1 -f image2pipe `
+		"$TmpFile" -y *>&1
 
-	$outStr = @(
-		$PSStyle.Foreground.Cyan, $Path, $PSStyle.Reset,
-		"→",
-		$PSStyle.Foreground.Green, $name, $PSStyle.Reset
-	)
+	$ocr = tesseract -l eng "$TmpFile" stdout 2>nul
 
-	Write-Host "$outStr"
-
-	return $name
-	<# $ff2 = ffmpeg -i "$tmp" -vf "crop=x=870:y=1050:w=680:h=50" -y "$tmp2" *>&1
-	$name = tesseract -l eng "$tmp2" stdout
-	$name = $name -is [array] ? $name[-1] : $name
-	$name = $name.Trim()
-	$name = $name -replace '\s+', ' '
+	$name = ($ocr -is [array] ? $ocr[-1] : $ocr) -replace '\s+', ' '
 	
-	# Remove-Item $tmp
-	# Remove-Item $tmp2
-
-	$outStr = @(
-		$PSStyle.Foreground.Cyan, $Path, $PSStyle.Reset,
+	<# $outStr = @(
+		$PSStyle.Foreground.Cyan, "Input: $Path", $PSStyle.Reset,
 		"→",
-		$PSStyle.Foreground.Green, $name, $PSStyle.Reset
-	)
+		$PSStyle.Foreground.Green, "OCR: $name", $PSStyle.Reset
+	) #>
 
-	Write-Host "$outStr"
+	$res = @{
+		Input = $Path
+		Song  = $name
+	}
 
-	return $name #>
+	Remove-Item -Path $TmpFile -ErrorAction SilentlyContinue
 
+	# Write-Host "$outStr"
+	# return $name
+
+	return $res
 }
+
+
+function Get-SongNames {
+	param (
+		$Names
+	)
+	
+	$i = 0
+	$Names | ForEach-Object {
+		$Path = $_
+		$job = Start-Job -Name "as_$i" -ScriptBlock ${function:Get-SongName} -ArgumentList $Path
+		$i++
+	}
+
+	<# 
+	$jobs | ForEach-Object {
+		$job = $_
+		$job | Wait-Job
+		$job | Receive-Job
+	} #>
+
+	<# while ($jobs.State -contains "Running") {
+		Start-Sleep -Milliseconds 100
+
+	} #>
+
+	while ($j2 = Wait-Job -Any -Name "as_*") {
+		
+		$j2 | Receive-Job -AutoRemoveJob -Wait
+		
+	}
+
+	# return $jobs
+}
+
