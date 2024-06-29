63499e51bb625411394e0412acbf551bc6285cf8
Read <decimation001@gmail.com>
2024-06-29 02:18:15 -0500

    Update

--- Scripts/PS-Audiosurf.ps1
+++ Scripts/PS-Audiosurf.ps1
@@ -8,34 +8,40 @@ function Get-SongName {
 	param (
 		[Parameter(Mandatory = $true)]
 		$Path,
-		$Seek = 15
+		$Seek = 15,
+		[switch]$DeleteTmp
 	)
 
-	$hsh = $Path.GetHashCode()
+	
+	$hsh = Get-Random
 	$TmpFile = "Frame_$hsh.png"
 
 
 	Write-Verbose "Temp file 1: $TmpFile"
 
-	$ff = ffmpeg -ss $Seek -i "$Path" -vf "select=eq(n\,34),crop=x=870:y=1050:w=1000:h=50" -vframes 1 -f image2pipe `
-		"$TmpFile" -y *>&1
+	$ff = ffmpeg -ss $Seek -i "$Path" -vf "select=eq(n\,34),crop=x=680:y=1045:w=860:h=35" -vframes 1 `
+		-f image2pipe "$TmpFile" -y *>&1
+
+	Write-Debug "$ff"
 
 	$ocr = tesseract -l eng "$TmpFile" stdout 2>nul
-
 	$name = ($ocr -is [array] ? $ocr[-1] : $ocr) -replace '\s+', ' '
 	
-	<# $outStr = @(
-		$PSStyle.Foreground.Cyan, "Input: $Path", $PSStyle.Reset,
-		"→",
-		$PSStyle.Foreground.Green, "OCR: $name", $PSStyle.Reset
-	) #>
-
 	$res = @{
 		Input = $Path
 		Song  = $name
 	}
 
-	Remove-Item -Path $TmpFile -ErrorAction SilentlyContinue
+
+	<# $p = Start-Process -FilePath $TmpFile -PassThru
+	Start-Sleep -Seconds 1
+	$p.Kill()
+	$p.Dispose() #>
+	# Start-Process $TmpFile -Wait
+	if ($DeleteTmp) {
+		Remove-Item -Path $TmpFile -ErrorAction SilentlyContinue
+	}
+
 
 	# Write-Host "$outStr"
 	# return $name
@@ -46,34 +52,31 @@ function Get-SongName {
 
 function Get-SongNames {
 	param (
-		$Names
+		$Paths
+
 	)
 	
 	$i = 0
-	$Names | ForEach-Object {
+	$jobs = @()
+	$Paths | ForEach-Object {
 		$Path = $_
+
+		Write-Debug "$Path"
+		# $Path = Resolve-Path -Path $Path
 		$job = Start-Job -Name "as_$i" -ScriptBlock ${function:Get-SongName} -ArgumentList $Path
 		$i++
+		$jobs += $job
 	}
 
-	<# 
-	$jobs | ForEach-Object {
-		$job = $_
-		$job | Wait-Job
-		$job | Receive-Job
-	} #>
 
-	<# while ($jobs.State -contains "Running") {
-		Start-Sleep -Milliseconds 100
-
-	} #>
-
+	$j = 0
 	while ($j2 = Wait-Job -Any -Name "as_*") {
 		
 		$j2 | Receive-Job -AutoRemoveJob -Wait
-		
+		Write-Progress -Activity "Processing" -Status "Received" -PercentComplete ($j++ / $i) `
+			-CurrentOperation "$output"
+	
 	}
 
-	# return $jobs
 }
 
